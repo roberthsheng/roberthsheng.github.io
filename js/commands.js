c88e8fd03ec8e95e377a2571dd176959798b6d6b
roberthsheng <roberthsheng@gmail.com>
2023-05-15 16:57:40 -0400

    init

---
+++ js/commands.js
@@ -0,0 +1,77 @@
+
+var twitter = "https://twitter.com/roberthsheng";
+var bookshelf = "https://www.goodreads.com/user/show/135356555-robert-sheng";
+var linkedin = "https://www.linkedin.com/in/robert-sheng/";
+var github = "https://github.com/roberthsheng";
+var email = 'mailto:robert.sheng@vanderbilt.edu';
+
+whois = [
+    "<br>",
+    "hey, i'm robert<",
+    "i study computer science and mathematics at vanderbilt university",
+    "my research interests include mathematical deep learning and cyber-physical systems",
+    "in my free time, i read, bike, and climb",
+    "reach out if you want to chat!",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'twitter        <a href="' + twitter + '" target="_blank">twitter/roberthsheng' + '</a>',
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/robert-sheng' + "</a>",
+    'github         <a href="' + github + '" target="_blank">github/roberthsheng' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          who am i?',
+    '<span class="command">whoami</span>         who are you?',
+    '<span class="command">bookshelf</span>      my library',
+    '<span class="command">social</span>         social networks',
+    '<span class="command">history</span>        view command history',
+    '<span class="command">help</span>           (you are here)',
+    '<span class="command">email</span>          email me',
+    '<span class="command">clear</span>          clear terminal',
+    '<span class="command">banner</span>         display header',
+    "<br>",
+];
+
+banner = [
+"         _nnnn_",
+"        dGGGGMMb",
+"       @p~qp~~qMb",
+"       M|@||@) M|",
+"       @,----.JM|",
+"      JS^\__/  qKL",
+"     dZP        qKRb",
+"    dZP          qKKb",
+"   fZP            SMMb",
+"   HZM            MMMM",
+"   FqM            MMMM",
+" __| `.        |\dS`qML",
+" |    `.       | `' \Zq",
+"_)      \.___.,|     .'",
+"\____   )MMMMMP|   .'",
+"     `-'       `--' hjm",
+    '<span class="color2">hello world, i\'m robert! welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">for a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
